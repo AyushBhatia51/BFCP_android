17fd07c5d7d70077503581720eb66ed956316a6e
Aryan Srivastava <118963887+aryan-srivastava8@users.noreply.github.com>
2023-05-18 23:34:26 +0530

    new Update

--- apt/app/src/main/java/com/example/apt/MainActivity.java
+++ apt/app/src/main/java/com/example/apt/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.apt;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.apt.Clint.SocketAsync;
-import com.example.apt.Clint.SocketManager;
-import com.example.apt.Clint.StreamInitializer;
 import com.example.apt.Clint.bfcp_client;
 import com.example.apt.JNIbfcp.*;
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,59 +17,74 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.concurrent.ExecutionException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     Thread Thread1 = null;
     EditText etIP, etPort;
     TextView tvMessages;
     EditText etMessage;
-    Button btnSend;
-    String SERVER_IP ="192.168.8.138";
+    Button btnSend,btnConnect;
+    String SERVER_IP ="192.168.8.130";
     bfcp_message message;
     Socket socket;
-    int SERVER_PORT =2345;
-    private SocketManager socketManager;
+    int SERVER_PORT =1234;
+//    private SocketManager socketManager;
     private bfcp_message bfM;
-    DataOutputStream dataOutputStream;
-    ObjectOutputStream objout;
+    PrintWriter writer;
+    BufferedReader reader;
     int error;
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        socketManager = new SocketManager(SERVER_IP, SERVER_PORT);
-        Button btnConnect = findViewById(R.id.btnConnect);
+//        socketManager = new SocketManager(SERVER_IP, SERVER_PORT);
+        btnConnect = findViewById(R.id.btnConnect);
         btnSend = findViewById(R.id.btnSend);
-        socketManager.connect();
+//        socketManager.connect();
         bfcp_participant_information initialpart = new bfcp_participant_information();
         bfcp_received_message_handler bfcpReceivedMessageHandler = new bfcp_received_message_handler();
         initialpart.bfcp_initialize_bfcp_participant(3001, 1001, "192.168.8.138", 2345, bfcpReceivedMessageHandler, 0);
         Log.e("parti", String.valueOf(initialpart.getConferenceID()));
         bfcp_client newcli = new bfcp_client();
-        btnConnect.setOnClickListener(v -> new Thread(() -> {
-            while (!socketManager.isConnected()) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+//        btnConnect.setOnClickListener(v -> new Thread(() -> {
+//            try {
+////
+//                bfM = newcli.bfcp_hello_participant(initialpart);
+//                socketManager.connect();
+//                socketManager.sendMessage(bfM.getBuffer());
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }).start());
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConnectToServerTask connectTask = new ConnectToServerTask();
+                connectTask.execute();
             }
-            try {
-                Log.d("socket", String.valueOf(socketManager.isConnected()));
-                bfM = newcli.bfcp_hello_participant(initialpart);
-                socketManager.sendMessage(bfM.getBuffer());
-            } catch (IOException e) {
-                e.printStackTrace();
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //                    bfM = newcli.bfcp_hello_participant(initialpart);
+                String message = etMessage.getText().toString();
+                SendMessageToServerTask sendTask = new SendMessageToServerTask();
+                sendTask.execute(message);
+
             }
-        }).start());
+        });
+
         /*btnSend.setOnClickListener(v -> new Thread(() -> {
             while (socketManager.isConnected()) {
                 try {
@@ -92,20 +108,92 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Disconnect socket when activity is destroyed
-        try {
-            socketManager.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        // Disconnect socket when activity is destroyed
+//        try {
+//            socketManager.disconnect();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 //public static bfcp_message received_message_from_server(){
 //        bfcp_message message1 = null;
 //    return message1;
 //
 //}
+
+
+
+    private class ConnectToServerTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                socket = new Socket(SERVER_IP, SERVER_PORT);
+                if(socket.isConnected()){
+                    Log.d("connected", "connected: ");
+                }
+                writer =  new PrintWriter(socket.getOutputStream(), true);
+                reader =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            if (socket != null && socket.isConnected()) {
+                tvMessages.setText("Connected to server.");
+                btnConnect.setEnabled(false);
+                btnSend.setEnabled(true);
+            } else {
+                tvMessages.setText("Connected to server.");
+            }
+        }
+    }
+
+    private class SendMessageToServerTask extends AsyncTask<String, Void, String> {
+
+        @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+        @Override
+        protected String doInBackground(String... messages) {
+            if (writer != null) {
+                try {
+                    String messageBytes = messages[0]; // Access the first element of the messages array
+                    writer.println(messageBytes);
+                    Log.d(TAG, "Sent message: " + new String(messageBytes));
+//                    byte[] buffer = new byte[1024];
+//                    byte[] data = new byte[1024];
+//                    BufferedInputStream bis = new BufferedInputStream(reader);
+//                    int bytesRead;
+//                    while((bytesRead = bis.read(buffer)) != -1){
+//                        data = Arrays.copyOf(buffer, bytesRead);
+
+//                    }
+                    String data = reader.readLine();
+
+                    return data;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String serverResponse) {
+            if (serverResponse != null) {
+//                for(int i = 0 ; i<serverResponse.length; i++) {
+                    Log.d("Server acknowledgment: ", "onPostExecute: " + serverResponse);
+//                }
+            } else {
+                Log.d("Server acknowledgment: ","Error communicating with the server.");
+            }
+        }
+    }
 }
+
 
